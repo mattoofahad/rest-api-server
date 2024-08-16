f37d4b85ae7f8d042924e90c343c830051ceb004
mattoofahadD3 <fahad.ulhassan@dcube.tech>
2024-08-17 00:27:53 +0500

    updating

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
+import json
 
 from logs import logger, log_execution_time
 
 app = FastAPI()
-
-
-class UserInputModel(BaseModel):
-    user_input: str
 
 
 @log_execution_time
@@ -17,14 +14,13 @@ class UserInputModel(BaseModel):
 async def twilio_message_endpoint(request: Request):
     data = await request.form()
     user_input = data.get("Body", "")
-    user_input = data.user_input
     reqUrl = "https://mattoofahad-whatsapp-endpoint.hf.space/message"
     headersList = {
-        "Accept": "*/*",
         "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-        "Content-Type": "application/x-www-form-urlencoded",
+        "accept": "application/json",
+        "Content-Type": "application/json",
     }
-    payload = f"Body={user_input}"
+    payload = json.dumps({"user_input": str(user_input)})
     response = requests.request("POST", reqUrl, data=payload, headers=headersList)
     logger.info(response.text)
     return PlainTextResponse(response.text)
