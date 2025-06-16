c7ce718f272315c414d01e55df6f05b4262a091f
Fahad Mattoo <63773040+mattoofahad@users.noreply.github.com>
2025-06-16 17:48:04 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv(), override=True)
 
-LOGGER_LEVEL = os.getenv("LOGGER_LEVEL")
+LOGGER_LEVEL = os.getenv("LOGGER_LEVEL", "DEBUG")
 READ_HF_TOKEN = os.getenv("READ_HF_TOKEN")
